5e38563347d0073f1a9a4f969f780e31ad1507ed
Pawel Sioda <pawel.sioda@gmail.com>
2021-11-07 14:48:30 +0100

    add Table and simple logic to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import {useState, useEffect} from 'react';
+import Table, {Column, OperationRecord} from "./components/Table";
+import data from "./data/data.json";
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      Hello, World!
-    </div>
-  );
+    const [income, setIncome] = useState([])
+    const [expenditures, setExpenditures] = useState([])
+
+    useEffect(() => {
+        setExpenditures(data.expenditures);
+        setIncome(data.income);
+    },[])
+    const expendituresRecords = expenditures.map(({name, amount, category}) => <OperationRecord name={name} amount={amount} category={category}/>)
+    const incomeRecords = income.map(({name, amount, category}) => <OperationRecord name={name} amount={amount} category={category}/>)
+
+    return (
+        <div className="App">
+            <Table>
+                <Column>
+                    {expenditures ? expendituresRecords : <div>Loading data...</div>}
+                </Column>
+                <Column>
+                    {income ? incomeRecords : <div>Loading data...</div>}
+                </Column>
+            </Table>
+        </div>
+    );
 }
 
 export default App;
